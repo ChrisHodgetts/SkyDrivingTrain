0014c038ae5d7fbcf7e1758bcfa2f26ecd695373
ChrisHodgetts <h006532c@student.staffs.ac.uk>
2014-11-07 00:51:14 +0000

    Player Movement
    Implemented player movement through the directional keypad. Removed Test
    change.

--- SkyDrivingTrain/SkyDrivingTrain/AppMain.cs
+++ SkyDrivingTrain/SkyDrivingTrain/AppMain.cs
@@ -13,9 +13,9 @@ namespace SkyDrivingTrain
 	{
 		private static GraphicsContext graphics;
 		private static Texture2D backgroundTexture;
+		private static Texture2D playerCharTexture;
 		private static Sprite background;
-		//LOOK HERE, I HAVE MADE A CHANGE
-		int REALLYBIGCHANGETOTHECODE = 99999;
+		private static Sprite playerChar;
 		
 		
 		public static void Main (string[] args)
@@ -35,6 +35,13 @@ namespace SkyDrivingTrain
 			// Set up the graphics system
 			graphics = new GraphicsContext();
 			
+			//Load in player character
+			playerCharTexture = new Texture2D("Application/assets/spikedShip.png", false);
+			playerChar = new Sprite(graphics, playerCharTexture);
+			playerChar.Position.X = 480;
+			playerChar.Position.Y = 277;
+			
+			
 			//NOTE: Screen size for PSVITA = 960x544
 			backgroundTexture = new Texture2D("/Application/assets/background.png", false);
 			background = new Sprite(graphics, backgroundTexture);
@@ -47,6 +54,21 @@ namespace SkyDrivingTrain
 		{
 			// Query gamepad for current state
 			var gamePadData = GamePad.GetData (0);
+			
+			
+			//Controls
+			if((gamePadData.Buttons & GamePadButtons.Left) != 0)
+				playerChar.Position.X = playerChar.Position.X -5;
+		
+			if((gamePadData.Buttons & GamePadButtons.Right) != 0)
+				playerChar.Position.X = playerChar.Position.X +5;
+			
+			if((gamePadData.Buttons & GamePadButtons.Up) !=0)
+				playerChar.Position.Y = playerChar.Position.Y -5;
+			
+			if((gamePadData.Buttons & GamePadButtons.Down) !=0)
+				playerChar.Position.Y = playerChar.Position.Y +5;
+			
 		}
 
 		public static void Render()
@@ -55,7 +77,9 @@ namespace SkyDrivingTrain
 			graphics.SetClearColor (0.0f, 0.0f, 0.0f, 0.0f);
 			graphics.Clear();
 			
+			//All sprites must be rendered here
 			background.Render();
+			playerChar.Render ();
 
 			// Present the screen
 			graphics.SwapBuffers();
